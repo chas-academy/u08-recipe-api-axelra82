ba68f729d5eb92bc5f0657f5ece35c10ef3ac591
Axel Roussille Åberg <axelra82@gmail.com>
2021-05-03 16:12:17 +0200

    chore: add cors middleware

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CORS
+{
+
+    public function handle(Request $request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: Content-type, X-Auth-Token, Authorization, Origin');
+        return $next($request);
+    }
+
+}
